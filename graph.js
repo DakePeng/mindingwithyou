ad5895b715e9375f1017992caaa2ac2c6728b8d0
Dake Peng <pengd@carleton.edu>
2024-05-16 12:26:12 -0500

    added some animation

--- graph.js
+++ graph.js
@@ -1,57 +1,61 @@
 const numNodes = 14
 // Generate random data for nodes and links
-var nodes = d3.range(numNodes).map(d => ({ id: d, isToggled : 0}));
-var links = d3.range(numNodes).map((d, i) => ({ source: i, target: 0 }));
-const radius = 300, r = 50;
+var nodes, links;
+var svg, link, node, nodeLabels;
+const graphR = 300, circleR = 50;
 var centerX, centerY;
 var simulation;
 const isChapterActive = [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1]
 const nodeColors = ["lightgray","#EDB4E9", "#EDB4E9", "#EDB4E9", "#EDB4E9", "#00C6AA", "#00C6AA", "#00C6AA", "#70D6FF", "#70D6FF", "#70D6FF", "#70D6FF", "#70D6FF", "#70D6FF"]
 
-// Create SVG elements
-var svg = d3.select("svg");
-var link = svg.selectAll(".link")
-    .data(links)
-    .enter().append("line")
-    .attr("class", "link");
-var node = svg.selectAll(".node")
-    .data(nodes)
-    .enter().append("circle")
-    .attr("class", "node")
-    .attr("r", 50)
-    .attr("fill", function(d){
-      if(isChapterActive[d.id] === 0) return nodeColors[0]
-      return nodeColors[d.id];
-    })
-    .on("mouseover", handleMouseOver)
-    .on("mouseout", handleMouseOut)// Increase node size
-    .on("click", toggleConnections)
-    .each(function(d) {
-      d.originalColor = d.color;
-  });
-
-var nodeLabels = svg.selectAll(".node-label")
-  .data(nodes)
-  .enter().append("text")
-  .attr("class", "node-label")
-  .attr("text-anchor", "middle")
-  .attr("dominant-baseline", "middle")
-  .attr("fill", "white")
-  .attr("dy", ".12em") // Adjust vertical alignment
-  .attr("dx", "-.05em") // Adjust vertical alignment
-  .text(function(d){if(d.id != 0) return d.id;})
-  .on("mouseover", function(d, i) {
-      if(isChapterActive[i.index] === 0) return
-      d3.select(this).style("cursor", "pointer");
-      findNode(i).node().dispatchEvent(new Event("mouseover"));
-  })
-  .on("click", function(d, i) {
-    findNode(i).node().dispatchEvent(new Event("click"));
-  });
-updateNodes()
-
+window.addEventListener('load', initialize);
 window.addEventListener('load', createSimulation);
 window.addEventListener('resize', createSimulation);
+
+function initialize(){
+  nodes = d3.range(numNodes).map(d => ({ id: d, isToggled : 0}));
+  links = d3.range(numNodes).map((d, i) => ({ source: i, target: 0 }));
+  svg = d3.select("svg");
+  link = svg.selectAll(".link")
+      .data(links)
+      .enter().append("line")
+      .attr("class", "link");
+
+  node = svg.selectAll(".node")
+      .data(nodes)
+      .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 50)
+      .attr("fill", function(d){
+        if(isChapterActive[d.id] === 0) return nodeColors[0]
+        return nodeColors[d.id];
+      })
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut)// Increase node size
+      .on("click", toggleConnections)
+      .each(function(d) {
+        d.originalColor = d.color;
+    });
+  
+  nodeLabels = svg.selectAll(".node-label")
+    .data(nodes)
+    .enter().append("text")
+    .attr("class", "node-label")
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "middle")
+    .attr("fill", "white")
+    .attr("dy", ".12em") // Adjust vertical alignment
+    .attr("dx", "-.05em") // Adjust vertical alignment
+    .text(function(d){if(d.id != 0) return d.id;})
+    .on("mouseover", function(d, i) {
+        if(isChapterActive[i.index] === 0) return
+        d3.select(this).style("cursor", "pointer");
+        findNode(i).node().dispatchEvent(new Event("mouseover"));
+    })
+    .on("click", function(d, i) {
+      findNode(i).node().dispatchEvent(new Event("click"));
+    });
+}
 
 // Function to update simulation center on window resize
 function createSimulation() {
@@ -84,8 +88,8 @@ function updateNodes(){
   const angleStep = (2 * Math.PI) / (numNodes - 1); // excluding the center node
   for (let i = 1; i < numNodes; i++) {
       const angle = (i - 1) * angleStep - Math.PI/2;
-      nodes[i].fx = centerX + radius * Math.cos(angle);
-      nodes[i].fy = centerY + radius * Math.sin(angle);
+      nodes[i].fx = centerX + graphR * Math.cos(angle);
+      nodes[i].fy = centerY + graphR * Math.sin(angle);
   }
 }
 
@@ -96,7 +100,6 @@ function findNode(i){
   return circle
 }
 
-// Handle mouseover event
 function handleMouseOver(d, i) {
   if(isChapterActive[i.id] === 0) return
   d3.select(this).style("cursor", "pointer");
@@ -104,15 +107,46 @@ function handleMouseOver(d, i) {
   d3.select(this).style("fill", "#ffd7b5");
 }
 
-// Handle mouseout event
 function handleMouseOut(d, i) {
   d3.select(this).style("cursor", "default");
   if(i.isToggled) return
   d3.select(this).style("fill", d.originalColor);
 }
 
-function toggleConnections(d, i) {
-  if(isChapterActive[i.id] === 0) return
-  i.isToggled = 1 - i.isToggled
-  d3.select(this).style("fill", "orange");  
+function toggleConnections(event, d) {
+  if(isChapterActive[d.id] === 0 || d.isToggled) return
+  d.isToggled = 1 - d.isToggled
+
+  node.transition()
+  .duration(1000)
+  .attr("cx", d.x)
+  .attr("cy", d.y)
+  .on("end", () => {
+    node.transition()
+    .duration(1000)
+    .attr("cx", centerX)
+    .attr("cy", centerY);
+  });
+
+  nodeLabels.transition()
+  .duration(1000)
+  .style("opacity", function(label){
+    if (label.id === d.id) return 1;
+    else return 0;
+  })
+  .on("end", () => {
+  nodeLabels.transition()
+  .duration(1000)
+  .attr("x", centerX)
+  .attr("y", centerY);
+  });
+  link.transition()
+  .duration(1000)
+  .style("opacity", 0);
+}
+
+function clicked(event, d) {
+  node.classed("active", false);
+  d3.select(this).classed("active", true);
+  
 }
